af5a4b9a6470c567010c40b116024739fa771e49
nsahin <nsahin@ebay.com>
2022-04-16 18:00:34 +0300

    created jpa repositories

---
+++ src/main/java/com/spring/stocktrackingsystem/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.spring.stocktrackingsystem.repository;
+
+import com.spring.stocktrackingsystem.model.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
+}
